6190130b551e4df3100b79ab3084e16af440c16f
gupta-nu <gupta.ananya@protonmail.com>
2025-05-15 20:00:22 +0530

    final changes

--- src/model.py
+++ src/model.py
@@ -4,8 +4,13 @@ def train_lgb(X_train, y_train):
     model = lgb.LGBMClassifier(
         objective='multiclass',
         num_class=len(set(y_train)),
-        learning_rate=0.1,
-        n_estimators=200
+        metric='multi_logloss',
+        boosting_type='gbdt',
+        num_leaves=31,
+        learning_rate=0.05,
+        min_data_in_leaf=10,
+        n_estimators=200,
+        verbose=-1
     )
     model.fit(X_train, y_train)
     return model
